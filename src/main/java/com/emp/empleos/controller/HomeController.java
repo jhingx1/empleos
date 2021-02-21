d957a569943979d84a20ee095b6b984e55f109a0
valerian <jean.huaringa@gmail.com>
2021-02-21 12:48:23 -0500

    controlador mas html

--- src/main/java/com/emp/empleos/controller/HomeController.java
+++ src/main/java/com/emp/empleos/controller/HomeController.java
@@ -1,5 +1,30 @@
 package com.emp.empleos.controller;
 
-public class HomeController {
+import java.util.Date;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String mostrarHome(Model model){
+//		model.addAttribute("mensaje","Hola Empleos App");
+//		model.addAttribute("fecha",new Date());
+		//210220
+		String nombre = "Auxiliar de Contabilidad";
+		Date fechaPub = new Date();
+		double salario = 9000;
+		boolean vigente = true;
+		
+		model.addAttribute("nombre",nombre);
+		model.addAttribute("fecha",fechaPub);
+		model.addAttribute("salario",salario);
+		model.addAttribute("vigente",vigente);
+		
+		return "home";
+	}
+	
 }
